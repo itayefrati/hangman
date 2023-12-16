63ef304b3239784c93141cbd99a2cce2d32a83d7
itayefrati <101872520+itayefrati@users.noreply.github.com>
2023-12-16 20:57:09 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,43 @@ HANGMAN_ASCII_ART = """welcome to the game hangman
  |_|  |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|
                       __/ |                      
                      |___/"""
-
+HANGMAN_PHOTOS = {1: "x-------x",
+    2: """x-------x
+|
+|
+|
+|
+|""",
+    3: """x-------x
+|       |
+|       0
+|
+|   
+|""",
+    4: """x-------x
+|       |
+|       0
+|       |
+|
+|""",
+    5: """x-------x
+|       |
+|       0
+|      /|\\
+|
+|""",
+    6:"""x-------x
+|       |
+|       0
+|      /|\\
+|      /
+|""",
+    7: """x-------x
+|       |
+|       0
+|      /|\\
+|      / \\
+|"""}
 # print(HANGMAN_ASCII_ART, '\n', MAX_TRIES)
 
 """tav = input("guess a letter: ")
@@ -84,11 +120,18 @@ def check_win(secret_word, old_letters_guessed):
         return False
 
 
+def print_hangman(num_of_tries):
+    return HANGMAN_PHOTOS[num_of_tries]
+
+
 def main():
-    data = ("self", "py", 1.543)
+    """data = ("self", "py", 1.543)
     format_string = "Hello %s %s learner, you have only %.1f units left before you master the course!"
-    print(format_string % data)
+    print(format_string % data)"""
+    num = 2
+    print(print_hangman(num))
 
 
 if __name__ == "__main__":
     main()
+    
